ad2cbb0c139b42e8980bf08306b82a710317b04a
vogel.jeanclaude <vogel.jeanclaude@cd6d7e76-efff-11de-b599-4d5b25800cad>
2010-01-01 18:19:43 +0000

    VS2005 project building en release + correction Install_vs2005.bat
    git-svn-id: https://jeukitue.googlecode.com/svn/trunk/moteur@41 cd6d7e76-efff-11de-b599-4d5b25800cad

--- ProjectFiles/Install_vs2005.bat
+++ ProjectFiles/Install_vs2005.bat
@@ -3,15 +3,15 @@ ECHO Copie des fichiers de l'IDE
 COPY VS2005\*.* ..\
 
 ECHO Copie des DLL nécessaires à l'exécution
-COPY ..\Libs\Agar-1.3.4\lib\ag_core.dll
-COPY ..\Libs\Agar-1.3.4\lib\ag_gui.dll
-COPY ..\Libs\fmodapi375win\api\fmod.dll
-COPY ..\Libs\FreeType\lib\freetype.dll
-COPY ..\Libs\Pre-built.2\lib\pthreadVC2.dll
-COPY ..\Libs\SDL-1.2.14\lib\SDL.dll
-COPY ..\Libs\SDL_image-1.2.10\lib\zlib1.dll
-COPY ..\Libs\SDL_image-1.2.10\lib\SDL_image.dll
-COPY ..\Libs\SDL_image-1.2.10\lib\libtiff-3.dll
-COPY ..\Libs\SDL_image-1.2.10\lib\libpng12-0.dll
-COPY ..\Libs\SDL_image-1.2.10\lib\jpeg.dll
-COPY ..\Libs\SDL_net-1.2.7\lib\SDL_net.dll
+COPY ..\Libs\Agar-1.3.4\lib\ag_core.dll ..
+COPY ..\Libs\Agar-1.3.4\lib\ag_gui.dll ..
+COPY ..\Libs\fmodapi375win\api\fmod.dll ..
+COPY ..\Libs\FreeType\lib\freetype.dll ..
+COPY ..\Libs\Pre-built.2\lib\pthreadVC2.dll ..
+COPY ..\Libs\SDL-1.2.14\lib\SDL.dll ..
+COPY ..\Libs\SDL_image-1.2.10\lib\zlib1.dll ..
+COPY ..\Libs\SDL_image-1.2.10\lib\SDL_image.dll ..
+COPY ..\Libs\SDL_image-1.2.10\lib\libtiff-3.dll ..
+COPY ..\Libs\SDL_image-1.2.10\lib\libpng12-0.dll ..
+COPY ..\Libs\SDL_image-1.2.10\lib\jpeg.dll ..
+COPY ..\Libs\SDL_net-1.2.7\lib\SDL_net.dll ..

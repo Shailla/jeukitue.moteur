3202f1770f051172fbe739b155d79424134c10b5
vogel.jeanclaude@gmail.com <vogel.jeanclaude@gmail.com@cd6d7e76-efff-11de-b599-4d5b25800cad>
2012-11-24 22:27:38 +0000

    Corrections mineures
    git-svn-id: https://jeukitue.googlecode.com/svn/trunk/moteur@157 cd6d7e76-efff-11de-b599-4d5b25800cad

--- sources/src/menu/Controller.h
+++ sources/src/menu/Controller.h
@@ -19,6 +19,7 @@ public:
 		LanceServeurMapAction,
 		CentralisateurAction,
 		ConfigurationAction,
+		QuitGameAction,
 		ConfigurationVideoAction,
 		ConfigurationJoueurAction,
 		ConfigCentralisateurAction,

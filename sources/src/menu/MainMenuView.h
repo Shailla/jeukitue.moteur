5e593b39bc6ac6153eb46c8c3ae0303df0944e2a
vogel.jeanclaude@gmail.com <vogel.jeanclaude@gmail.com@cd6d7e76-efff-11de-b599-4d5b25800cad>
2012-11-24 22:27:02 +0000

    Corrections mineures
    git-svn-id: https://jeukitue.googlecode.com/svn/trunk/moteur@155 cd6d7e76-efff-11de-b599-4d5b25800cad

--- sources/src/menu/MainMenuView.h
+++ sources/src/menu/MainMenuView.h
@@ -13,6 +13,7 @@ class MainMenuView : public View
     AG_Button* _buttonOuvrirScene;
 	AG_Button* _buttonMultijoueurs;
     AG_Button* _buttonConfiguration;
+    AG_Button* _buttonQuitGame;
     AG_Button* _About;
     AG_Button* _debug;
     AG_Button* _buttonQuitter;
